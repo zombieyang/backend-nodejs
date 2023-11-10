b9f11943c61ef8a53eba2922f2ffa0aa6abf5a18
僵尸浩 <348063288@qq.com>
2023-11-10 15:01:36 +0800

    Update windows_64.cmd

--- windows_64.cmd
+++ windows_64.cmd
@@ -5,7 +5,7 @@ md puerts-node\nodejs\deps\uv\include
 md puerts-node\nodejs\deps\v8\include
 
 md puerts-node\nodejs\Lib\Win64\
-copy node\out\Release\libnode.dll .\puerts-node\nodejs\Lib\Win64\
-copy node\out\Release\libnode.exp .\puerts-node\nodejs\Lib\Win64\
-copy node\out\Release\libnode.lib .\puerts-node\nodejs\Lib\Win64\
-copy node\out\Release\libnode.pdb .\puerts-node\nodejs\Lib\Win64\
+copy node\out\Debug\libnode.dll .\puerts-node\nodejs\Lib\Win64\
+copy node\out\Debug\libnode.exp .\puerts-node\nodejs\Lib\Win64\
+copy node\out\Debug\libnode.lib .\puerts-node\nodejs\Lib\Win64\
+copy node\out\Debug\libnode.pdb .\puerts-node\nodejs\Lib\Win64\
