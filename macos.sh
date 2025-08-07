b7ab9fe8464720d49e30081ddcd4cbe850b4fb68
zombieyang <zombieyang@tencent.com>
2025-08-07 16:37:34 +0800

    24.5.0

--- macos.sh
+++ macos.sh
@@ -11,9 +11,9 @@ git fetch origin v$VERSION
 git checkout v$VERSION
 
 echo "=====[Patching Node.js]====="
-node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/lib_uv_add_on_watcher_queue_updated_v$VERSION.patch
-node $WORKSPACE/node-script/add_arraybuffer_new_without_stl.js deps/v8
-node $WORKSPACE/node-script/make_v8_inspector_export.js
+# node $WORKSPACE/node-script/do-gitpatch.js -p $WORKSPACE/patchs/lib_uv_add_on_watcher_queue_updated_v$VERSION.patch
+# node $WORKSPACE/node-script/add_arraybuffer_new_without_stl.js deps/v8
+# node $WORKSPACE/node-script/make_v8_inspector_export.js
 
 echo "=====[Building Node.js]====="
 
